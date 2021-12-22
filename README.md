6eb27520eab560eaadbac233511d2528adf4b4c6
Daniel Klein <daniel@hellolanding.com>
2021-12-22 14:52:16 -0600

    a bunch of cleanup (including readme fleshing out)

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
-# Hello world javascript action
+# Instrument CI GitHub Action
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+When someone merges a pull request, this action sends a custom event to New Relic with info about the PR like...
+* the PR author
+* how many lines were added or deleted
+* when the PR was originally created
+
+
+
+There is huge potential in the future for the following:
+1. increasing the amount of info we put into the custom PR event
+2. allowing this Action to send info to platforms other than New Relic
+3. increasing the scope of things this Action supports, such as PR events other than merges and even tracking events on other stuff involved in the dev process, like how long individual CI steps take!
 
 ## Inputs
 
-## `who-to-greet`
+No inputs are currently supported.
 
-**Required** The name of the person to greet. Default `"World"`.
+*HOWEVER,* in order for reporting to New Relic to work, you must pass the environment variable `NEW_RELIC_LICENSE_KEY` to this Action with the value being your New Relic license key.
+Likewise, it is highly recommended that you pass the `NEW_RELIC_APP_NAME` environment variable to customize the application for which the events are reported. Otherwise, it will default to `My Application`.
 
 ## Outputs
 
-## `time`
-
-The time we greeted you.
+This action currently does not return any outputs.
 
 ## Example usage
 
-uses: actions/hello-world-javascript-action@v1.1
-with:
-  who-to-greet: 'Mona the Octocat'
+on:
+  pull_request:
+    types: [closed]
+jobs:
+  report_pr_event:
+    runs-on: ubuntu-latest
+    name: send PR event to New Relic
+    steps:
+      - name: send event
+        uses: danielmklein/instrument-ci@main
+        env:
+          NEW_RELIC_LICENSE_KEY: ${{ secrets.NEW_RELIC_LICENSE_KEY }}
+          NEW_RELIC_APP_NAME: "GitHub Actions Test"
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// TODOs:
-// - figure out how to make new relic app name configurable via param
-// - figure out how to put repo name into event... maybe also base branch
-// - figure out how to make new relic license key configurable via param
-// - figure out how to structure these events
-// - cleanup readme
-
 const newrelic = require('newrelic')
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
   var context = github.context
-  // console.log(context)
 
   if (context.eventName != "pull_request") {
     var message = "This action currently only supports pull request events"
@@ -35,14 +27,6 @@ try {
   const pull_request = context.payload.pull_request;
   const repo = context.payload.repository;
 
-  // console.log(pull_request)
-  // console.log("PR# " + pull_request.number)
-  // console.log("created_at " + pull_request.created_at)
-  // console.log("now: " + Date.now())
-  // console.log("total_additions: " + pull_request.additions)
-  // console.log("total_additions: " + pull_request.deletions)
-  // console.log("total_changed_files: ", pull_request.changed_files)
-
   var event_name = "pull_request_event"
   var event_attributes = {
     repo_name: repo.name,
@@ -58,22 +42,6 @@ try {
   console.log(`sending ${event_name} event to New Relic with the following attributes:`)
   console.log(event_attributes)
   newrelic.recordCustomEvent(event_name, event_attributes)
-
-  // core.setOutput('additions', pr.additions);
-  // core.setOutput('deletions', pr.deletions);
-  // core.setOutput('commits', pr.commits);
-  // core.setOutput('assignee', pr.assignee);
-  // core.setOutput('number', pr.number);
-  // core.setOutput('title', pr.title);
-  // core.setOutput('body', pr.body);
-  // core.setOutput('created_at', pr.created_at);
-  // core.setOutput('updated_at', pr.updated_at);
-  // core.setOutput('url', pr.html_url);
-  // core.setOutput('base_branch', pr.base.ref);
-  // core.setOutput('base_commit', pr.base.sha);
-  // core.setOutput('head_branch', pr.head.ref);
-  // core.setOutput('head_commit', pr.head.sha);
-  // core.setOutput('draft', pr.draft);
 } catch (error) {
   core.setFailed(error.message);
 }
